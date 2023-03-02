c64b2d2ffcba8fe7cfbe260c544d536fc70f52db
Nemanja Malesija <malesija13@gmail.com>
2023-03-02 22:39:41 +0100

    Complete divide operation in the reducer

--- src/reducer.ts
+++ src/reducer.ts
@@ -26,11 +26,9 @@ const reducer = (state: calculator, action: ACTIONS): any => {
         result = Number(state.currentOperand) * Number(state.previousOperand);
       }
       if (payload === '/' && state.currentOperand !== '0') {
-        result = Number(state.currentOperand) * Number(state.previousOperand);
-      }
-      return alert("You can't divide by 0");
-
-      return { ...state, currentOperand: result, previousOperand: '' };
+        result = Number(state.currentOperand) / Number(state.previousOperand);
+        return { ...state, currentOperand: result, previousOperand: '' };
+      } else alert("You can't divide by 0");
     }
 
     return {
