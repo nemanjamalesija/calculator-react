7dbdcf608f6826ac17f17ba6eec0fef609192cee
Nemanja Malesija <malesija13@gmail.com>
2023-03-02 22:27:56 +0100

    Handle addition operation in reducer

--- src/reducer.ts
+++ src/reducer.ts
@@ -12,6 +12,27 @@ const reducer = (state: calculator, action: ACTIONS): any => {
     };
   }
 
+  if (type === 'SELECT_OPERATION') {
+    console.log(state.currentOperand, state.previousOperand);
+
+    const prevOperandUpdate = Number(state.currentOperand);
+
+    if (state.currentOperand && state.previousOperand) {
+      let result;
+      if (payload === '+') {
+        result = Number(state.currentOperand) + Number(state.previousOperand);
+      }
+
+      return { ...state, currentOperand: result, previousOperand: '' };
+    }
+
+    return {
+      ...state,
+      currentOperand: '',
+      previousOperand: prevOperandUpdate,
+    };
+  }
+
   return { ...state };
 };
 
